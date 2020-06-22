f09932dba981f21ff4834c009454618d8f302b19
rakibi-alaa <rakibi.alae@gmail.com>
2020-06-22 09:38:10 +0100

    project init and migrations

---
+++ database/migrations/2020_06_22_081025_create_offers_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateOffersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('offers', function (Blueprint $table) {
+            $table->string('ref')->primary();
+            $table->string('title');
+            $table->unsignedBigInteger('sector_id');
+            $table->unsignedBigInteger('education_level_id');
+            $table->string('localisation');
+            $table->timestamp('expires_in');
+            $table->foreign('sector_id')->references('id')->on('sectors');
+            $table->foreign('education_level_id')->references('id')->on('education_levels');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('offers');
+    }
+}
